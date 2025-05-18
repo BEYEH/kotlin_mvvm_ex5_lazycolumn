8fdc3420e29e11706a356698be1a5564f0e32c63
kells\KellsPing <kellsping@gmail.com>
2025-05-18 22:57:29 +0800

    Update ui.

--- app/src/main/java/com/example/kotlin_mvvvm_ex5_lazy_column/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_mvvvm_ex5_lazy_column/MainActivity.kt
@@ -12,36 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlin_mvvvm_ex5_lazy_column.ui.theme.Kotlin_mvvvm_ex5_lazy_columnTheme
+import com.example.kotlin_mvvvm_ex5_lazy_column.view.ListDemo
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Kotlin_mvvvm_ex5_lazy_columnTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ListDemo()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Kotlin_mvvvm_ex5_lazy_columnTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/kotlin_mvvvm_ex5_lazy_column/view/ListDemo.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin_mvvvm_ex5_lazy_column.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ListDemo() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "List Demo")
+    }
+}
