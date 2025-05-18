d4add7085ef5cf2195578136243ea294bb52a5f0
kells\KellsPing <kellsping@gmail.com>
2025-05-18 22:23:56 +0800

    Create app.

---
+++ app/src/main/java/com/example/kotlin_mvvvm_ex5_lazy_column/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.kotlin_mvvvm_ex5_lazy_column
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_mvvvm_ex5_lazy_column.ui.theme.Kotlin_mvvvm_ex5_lazy_columnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Kotlin_mvvvm_ex5_lazy_columnTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Kotlin_mvvvm_ex5_lazy_columnTheme {
+        Greeting("Android")
+    }
+}
